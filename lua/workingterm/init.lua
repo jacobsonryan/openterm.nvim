dc5c4fc654648f2cfc5c1280f9f3855989ab5b8d
jacobsonryan <ryanhjacobson@gmail.com>
2022-12-18 17:32:08 -0500

    setup func

--- lua/workingterm/init.lua
+++ lua/workingterm/init.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+M.setup = function(key)
+  if(key)
+    then
+      keymap('t', key.key, [[<C-\><C-n>]], opts)
+  end
+end
+
 M.open = function()
   local cwd = vim.fn.getcwd()
   vim.api.nvim_command("tabnew | terminal")
