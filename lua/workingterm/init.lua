ea34d0d27e00998946d340e68f72b1c688068313
Ryan Jacobson <ryanhjacobson@gmail.com>
2023-05-18 11:08:25 -0400

    vsplit

--- lua/workingterm/init.lua
+++ lua/workingterm/init.lua
@@ -17,4 +17,11 @@ M.open = function()
   vim.api.nvim_command(command)
 end
 
+M.vsplit = function()
+  local cwd = vim.fn.getcwd()
+  vim.api.nvim_command("vertical | terminal")
+  local command = ':call jobsend(b:terminal_job_id, "cd ' .. cwd .. '\\n")'
+  vim.api.nvim_command(command)
+end
+
 return M
--- plugin/workingterm.lua
+++ plugin/workingterm.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_command("command! OpenWorkingTerm lua require'workingterm'.open()")
+vim.api.nvim_command("command! OpenWorkingTermVSplit lua require'workingterm'.vsplit()")
